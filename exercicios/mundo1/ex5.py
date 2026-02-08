e88abbb71c155e7b39ba79dbe26906f5350ecbcd
Adriano Baumg <154255833+adrianobaumg@users.noreply.github.com>
2026-02-08 18:34:56 +0000

    add ex

---
+++ exercicios/mundo1/ex5.py
@@ -0,0 +1,3 @@
+#Faça um programa que leia um número Inteiro e mostre na tela o seu sucessor e seu antecessor.
+num = int(input("Escreva um número: "))
+print(f"Seu antecessor eh {num-1} sucessor eh {num+1}")
