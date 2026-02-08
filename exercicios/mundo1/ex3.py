1dd390a4a45f47bca0b47ae245cb1f2eeeeaae97
Adriano Baumg <154255833+adrianobaumg@users.noreply.github.com>
2026-02-08 18:26:06 +0000

    add ex3

---
+++ exercicios/mundo1/ex3.py
@@ -0,0 +1,5 @@
+# Exercício Python 003: Crie um programa que leia dois números e mostre a soma entre eles.
+num_1 = float(input("Escreva um número: "))
+num_2 = float(input("Escreva outro número: "))
+soma = num_1+num_2
+print(f"a soma entre {num_1} e {num_2} eh {soma}")
