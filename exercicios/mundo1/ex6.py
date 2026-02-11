7b0417995883b9a437b865ee4cc46138e930d27c
Adriano Baumg <154255833+adrianobaumg@users.noreply.github.com>
2026-02-11 21:07:45 +0000

    add ex

---
+++ exercicios/mundo1/ex6.py
@@ -0,0 +1,3 @@
+# Crie um algoritmo que leia um número e mostre o seu dobro, o seu triplo e sua raiz quadrada:
+n = float(input("Digite um numero: "))
+print(f"Seu dobro eh {n*2} . Seu triplo eh {n*3} e sua raiz quadrada eh {n**(1/2)}")
